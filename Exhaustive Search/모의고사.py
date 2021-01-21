2596593e07c1d07945ae10c81300acd3755bebd4
seungyoonkim66 <tmddbs1998@naver.com>
2021-01-21 23:20:27 +0900

    모의고사 complete

--- Exhaustive Search/모의고사.py
+++ Exhaustive Search/모의고사.py
@@ -12,19 +12,25 @@
 
 def solution(answers):
     answer = []
-
     s1 = [1,2,3,4,5]
     s2 = [2,1,2,3,2,4,2,5]
     s3 = [3,3,1,1,2,2,4,4,5,5]
+    scores = [0,0,0]
 
-    max = 0
-    for student in[s1, s2, s3]:
-        score = 0
-        for i in range(len(answers)):
-            if student[i%len(s1)] == answers[i] :
-                score += 1
-        if score >= max:
-            answer.append([s1,s2,s3].index(student)+1)
+    for i in range(len(answers)):
+        if answers[i] == s1[i%len(s1)]:
+            scores[0] += 1
+
+        if answers[i] == s2[i%len(s2)]:
+            scores[1] += 1
+
+        if answers[i] == s3[i%len(s3)]:
+            scores[2] += 1
+    
+
+    for j in range(3):
+        if scores[j] == max(scores):
+            answer.append(j+1)
 
     return sorted(answer)
 
